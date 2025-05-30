a89b50d6965447cc793d2034e9f9381bbf5925b8
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-05-30 17:57:34 +0800

    test

---
+++ example1/1.demo.sh
@@ -0,0 +1,14 @@
+https://3ega4eier9.execute-api.ap-south-1.amazonaws.com/dev/subscribe
+
+{
+  "email": "bhrth.dsra2@gmail.com"
+}
+
+https://3ega4eier9.execute-api.ap-south-1.amazonaws.com/dev/create-event
+
+
+{
+  "title": "Tech Meetup",
+  "date": "2024-12-01",
+  "description": "A gathering of tech enthusiasts!"
+}
